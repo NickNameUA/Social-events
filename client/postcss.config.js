5b772a4b6567d8bf31279b0a6f1d90738a6f03e7
Bogdan Tyron <bogdan.tyron3@gmail.com>
2023-11-08 14:31:43 +0200

    install dependencies

---
+++ client/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    "postcss-sorting": {
+      "properties-order": "alphabetical",
+      "unspecified-properties-position": "bottom",
+    },
+    "postcss-nested": {},
+    "postcss-conditionals": {},
+    autoprefixer: {},
+  },
+};
